6cdca3a6da22336ec7f90b333758526a79497b1f
Subhi YD <subhy809fr@gmail.com>
2023-07-14 00:38:52 +0200

    v1.1.0-create findByNameContains with pagination and search with keyword

--- src/main/java/com/uyghur/java/hospital/hospital/controller/PatientController.java
+++ src/main/java/com/uyghur/java/hospital/hospital/controller/PatientController.java
@@ -2,9 +2,13 @@ package com.uyghur.java.hospital.hospital.controller;
 
 import com.uyghur.java.hospital.hospital.entity.Patient;
 import com.uyghur.java.hospital.hospital.repository.PatientRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -19,9 +23,24 @@ public class PatientController {
     }
 
     @GetMapping("/index")
-    public String index(Model model) {
-        List<Patient> list = repository.findAll();
-        model.addAttribute("list", list);
+    public String index(Model model,
+                        @RequestParam(name = "page", defaultValue = "0") int page,
+                        @RequestParam(name = "size", defaultValue = "10") int size,
+                        @RequestParam(name = "keyword", defaultValue = "") String keyword) {
+
+        //Page<Patient> list = repository.findAll(PageRequest.of(page, size));
+        Page<Patient> list = repository.findByNameContains(keyword, PageRequest.of(page, size));
+
+        model.addAttribute("list", list.getContent());
+        model.addAttribute("pages", new int[list.getTotalPages()]);
+        model.addAttribute("currentPage", page);
+        model.addAttribute("keyword", keyword);
+
         return "/list/patients";
+    }
+
+    @GetMapping("/patients")
+    public ResponseEntity<List<Patient>> retrieveAllPatients() {
+        return ResponseEntity.ok(repository.findAll());
     }
 }
--- src/main/java/com/uyghur/java/hospital/hospital/repository/PatientRepository.java
+++ src/main/java/com/uyghur/java/hospital/hospital/repository/PatientRepository.java
@@ -2,7 +2,17 @@ package com.uyghur.java.hospital.hospital.repository;
 
 
 import com.uyghur.java.hospital.hospital.entity.Patient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+
+    Page<Patient> findByNameContains(String name,
+                                     Pageable pageable);
+
+    @Query("SELECT patient FROM Patient patient WHERE patient.name LIKE :name")
+    Page<Patient> search(@Param("name") String name, Pageable pageable);
 }
